bfa9081b6cc9c436216150580044be9e81661165
qcrao <qcrao91@gmail.com>
2024-05-23 23:59:28 +0800

    mobile succeed to save png

--- src/BlockShareCardComponent.jsx
+++ src/BlockShareCardComponent.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { Button, Menu, MenuItem, Popover } from "@blueprintjs/core";
+import { shareAndDownloadImage } from "./download.js";
 
 function BlockShareCardComponent() {
+  const handleClick = (item) => {
+    console.log(`Clicked on ${item}`);
+  };
+
   const menu = (
     <Menu>
-      <MenuItem icon="mobile-phone" text="mobile" />
-      <MenuItem icon="desktop" text="desktop" />
+      <MenuItem
+        icon="mobile-phone"
+        text="mobile"
+        onClick={() => shareAndDownloadImage()}
+      />
+      <MenuItem
+        icon="desktop"
+        text="desktop"
+        onClick={() => handleClick("desktop")}
+      />
     </Menu>
   );
 
