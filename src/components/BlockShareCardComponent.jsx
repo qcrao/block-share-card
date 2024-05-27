662711ad9a75c8384c596cb212934ec92da28309
qcrao <qcrao91@gmail.com>
2024-05-27 22:11:30 +0800

    update

--- src/components/BlockShareCardComponent.jsx
+++ src/components/BlockShareCardComponent.jsx
@@ -1,26 +1,48 @@
-import React from "react";
-import { Button, Menu, MenuItem, Popover } from "@blueprintjs/core";
+import React, { useState } from "react";
+import { Button, Menu, MenuItem, Popover, Tooltip } from "@blueprintjs/core";
 import { shareAndDownloadImage } from "../download.js";
 
 function BlockShareCardComponent() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopover = () => setIsOpen(!isOpen);
+  const closePopover = () => setIsOpen(false);
+
   const menu = (
     <Menu>
       <MenuItem
         icon="mobile-phone"
-        text="mobile"
-        onClick={() => shareAndDownloadImage(true)}
+        text="Mobile"
+        onClick={() => {
+          shareAndDownloadImage(true);
+          closePopover();
+        }}
       />
       <MenuItem
         icon="desktop"
-        text="desktop"
-        onClick={() => shareAndDownloadImage(false)}
+        text="Desktop"
+        onClick={() => {
+          shareAndDownloadImage(false);
+          closePopover();
+        }}
       />
     </Menu>
   );
 
   return (
-    <Popover content={menu} placement="bottom">
-      <Button className="bp3-button bp3-minimal" icon="send-to" text="" />
+    <Popover
+      content={menu}
+      isOpen={isOpen}
+      onInteraction={(state) => setIsOpen(state)}
+      position="bottom">
+      <Tooltip content="Share block" placement="bottom">
+        <Button
+          className="bp3-button bp3-minimal"
+          icon="send-to"
+          text=""
+          onClick={togglePopover}
+        />
+      </Tooltip>
     </Popover>
   );
 }
